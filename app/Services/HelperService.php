9a0689a4007f95b68b1431ea73f4400833895a09
Dusk252 <19530680+Dusk252@users.noreply.github.com>
2020-03-22 22:04:48 +0000

    fix filehash could be repeated as long as audio payload was the same and we don't want that

--- app/Services/HelperService.php
+++ app/Services/HelperService.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use getID3;
+use getid3_lib;
+use getid3_writetags;
 
 class HelperService
 {
@@ -22,6 +24,57 @@ class HelperService
     {
         $info = $this->getID3->analyze($path);
         $fileHash = array_get($info, 'md5_data');
-        return $fileHash;
+        $salt = "";
+        $dataformat = array_get($info, 'audio.dataformat');
+        if ($dataformat != null) {
+            if ($dataformat == 'mp3' || $dataformat == 'm4a')
+                $salt = array_get($info, "id3v1.comments.comment", [null])[0];
+            else if ($dataformat == 'flac' || $dataformat == 'vorbis' || $dataformat == 'ogg')
+                $salt = array_get($info, "tags.vorbiscomment.comment", [null])[0];
+            
+            if ($salt == null || substr($salt, 0, 5) != "salt=") {
+                $salt = "salt=".uniqid('', true);
+                $this->writeSaltToFile($path, $salt, $dataformat, $info);
+            }
+        }
+        return md5($fileHash.$salt);
+    }
+
+    private function writeSaltToFile(string $path, string $salt, string $dataformat, array $info): bool
+    {
+        $getID3 = new getID3;
+        $getID3->setOption(array('encoding'=>'UTF-8'));
+
+        getid3_lib::IncludeDependency(GETID3_INCLUDEPATH.'write.php', __FILE__, true);
+
+        $tagwriter = new getid3_writetags;
+        $tagwriter->filename = $path;
+
+        $tagwriter->tag_encoding   = 'UTF-8';
+        $tagwriter->remove_other_tags = false;
+        $tagwriter->overwrite_tags = true;
+
+        $tagdata = array(
+            'comment' => array($salt)
+        );
+
+        if ($dataformat == 'mp3')
+            $tagwriter->tagformats = array('id3v1');
+        else if ($dataformat == 'flac') {
+            $tagwriter->tagformats = array('metaflac');
+            $tagdata = array_merge(array_get($info, 'flac.comments'), array(
+                'comment' => array($salt)
+            ));
+        }
+        else if ($dataformat == 'vorbis' || $dataformat == 'ogg')
+            $tagwriter->tagformats = array('vorbiscomment');
+        else
+            return false;
+
+        $tagwriter->tag_data = $tagdata;
+        if ($tagwriter->WriteTags()) {
+            return true;
+        }
+        return false;
     }
 }
--- database/migrations/2020_03_22_084233_change_song_id_to_filehash.php
+++ database/migrations/2020_03_22_084233_change_song_id_to_filehash.php
@@ -9,7 +9,7 @@ class ChangeSongIdToFileHash extends Migration
 
     public function __construct()
     {
-        $this->getID3 = new getID3();
+        $this->getID3 = new getID3;
     }
 
     /**
@@ -72,11 +72,18 @@ class ChangeSongIdToFileHash extends Migration
     
         foreach ($results as $result){
             $new_id = $this->getHashFromPath($result->path);
-            DB::table('songs')
+            if ($new_id == null) {
+                DB::table('songs')
+                    ->where('id',$result->id)
+                    ->delete();
+            }
+            else {
+                DB::table('songs')
                 ->where('id',$result->id)
                 ->update([
                     "id" => $new_id
             ]);
+            }
         }
 
         Schema::table('playlist_song', function (Blueprint $table) {
@@ -89,13 +96,54 @@ class ChangeSongIdToFileHash extends Migration
         });
     }
 
-    private function getHashFromData(string $path): string {
+    private function getHashFromData(string $path): ?string {
         $info = $this->getID3->analyze($path);
         $fileHash = array_get($info, 'md5_data');
-        return $fileHash;
+        $salt = uniqid('', true);
+        if ($this->writeSaltToFile($path, $salt, array_get($info, 'audio.dataformat') ?: 'mp3'))
+            return md5($fileHash.$salt);
+        else
+            return null;
     }
 
     private function getHashFromPath(string $path): string {
         return md5(config('app.key').$path);
+    }
+
+    private function writeSaltToFile(string $path, string $salt, string $dataformat, array $info): bool
+    {
+        $getID3 = new getID3;
+        $getID3->setOption(array('encoding'=>'UTF-8'));
+
+        getid3_lib::IncludeDependency(GETID3_INCLUDEPATH.'write.php', __FILE__, true);
+
+        $tagwriter = new getid3_writetags;
+        $tagwriter->filename = $path;
+
+        $tagdata = array(
+            'comment' => array($salt)
+        );
+
+        if ($dataformat == 'mp3')
+            $tagwriter->tagformats = array('id3v1');
+        else if ($dataformat == 'flac') {
+            $tagwriter->tagformats = array('metaflac');
+            $tagdata = array_merge(array_get($info, 'flac.comments'), array(
+                'comment' => array($salt)
+            ));
+        }
+        else if ($dataformat == 'vorbis' || $dataformat == 'ogg')
+            $tagwriter->tagformats = array('vorbiscomment');
+        else
+            return false;
+		$tagwriter->overwrite_tags = true;
+        $tagwriter->tag_encoding   = 'UTF-8';
+        $tagwriter->remove_other_tags = false;
+
+        $tagwriter->tag_data = $tagdata;
+        if ($tagwriter->WriteTags()) {
+            return true;
+        }
+        return false;
     }
 }
